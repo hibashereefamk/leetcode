7cadfc48f9e81d78e29a3349f8c26e80f88189a2
hiba shereefa mk <hibashareefamk@gmail.com>
2025-12-29 23:17:47 +0530

    Update 177. Nth Highest Salary.sql

--- 177. Nth Highest Salary.sql
+++ 177. Nth Highest Salary.sql
@@ -1,14 +1,18 @@
 CREATE OR REPLACE FUNCTION NthHighestSalary(N INT)
 RETURNS TABLE (Salary INT) AS $$
 BEGIN
-  RETURN QUERY
-  SELECT sub.salary
-  FROM (
-      SELECT DISTINCT e.salary
-      FROM Employee e
-      ORDER BY e.salary DESC
-      OFFSET N - 1
-      LIMIT 1
-  ) sub;
+  IF N <= 0 THEN
+      RETURN QUERY SELECT NULL::INT;
+  ELSE
+      RETURN QUERY
+      SELECT sub.salary
+      FROM (
+          SELECT DISTINCT e.salary
+          FROM Employee e
+          ORDER BY e.salary DESC
+          OFFSET N - 1
+          LIMIT 1
+      ) sub;
+  END IF;
 END;
 $$ LANGUAGE plpgsql;
