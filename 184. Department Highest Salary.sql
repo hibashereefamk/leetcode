a7bb6771459662f56d279692b5cf7b95a9f517d1
hiba shereefa mk <hibashareefamk@gmail.com>
2026-01-03 18:43:30 +0530

    Create 184. Department Highest Salary.sql

---
+++ 184. Department Highest Salary.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+SELECT d.name AS Department,e.name AS Employee,e.salary AS Salary FROM Employee e
+JOIN Department d ON d.id =e.departmentId 
+WHERE (e.departmentId, e.salary) IN (SELECT departmentId,MAX(salary) FROM Employee 
+GROUP BY departmentId )
